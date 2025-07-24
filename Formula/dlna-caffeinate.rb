13b478395d32962995efd0213fe649fba7788907
oochernyshev <46565888+oochernyshev@users.noreply.github.com>
2025-07-25 00:04:51 +0200

    Update dlna-caffeinate.rb

--- Formula/dlna-caffeinate.rb
+++ Formula/dlna-caffeinate.rb
@@ -1,15 +1,16 @@
 class DlnaCaffeinate < Formula
   desc "Keep Mac awake only while MiniDLNA is running"
   homepage "https://github.com/oochernyshev/dlna-caffeinate"
-  url "https://github.com/oochernyshev/dlna-caffeinate/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "09d34ae6b7cd5dc8c988dec529ca70c965b3a9a0526cec8a4600a9f88593c8c5"
-  version "1.0.0"
+  url "https://github.com/oochernyshev/dlna-caffeinate/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "adb906e0ddca3f38d4a9715aea1fddfde20eab04955c0750a86f21bc09b643aa"
+  version "1.0.1"
 
   def install
-    bin.install "dlna-caffeinate"
+    bin.install "dlna-caffeinate", "dlna-caffeinate-monitor"
   end
 
   test do
     system "#{bin}/dlna-caffeinate", "status"
   end
 end
+
